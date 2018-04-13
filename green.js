707a55f0092d9f7f32c757acee1e57e08a3e4869
rb250471 <robert.baboi@ncr.com>
2018-04-13 14:26:44 +0100

    removed anger

--- green.js
+++ green.js
@@ -21,7 +21,5 @@
     beautify("icon-blue-anime icon-sm","https://raw.githubusercontent.com/jenkinsci/greenballs-plugin/master/src/main/webapp/16x16/green_anime.gif");
     beautify("icon-blue-anime icon-lg","https://raw.githubusercontent.com/jenkinsci/greenballs-plugin/master/src/main/webapp/32x32/green_anime.gif");
     beautify("icon-blue-anime icon-xlg","https://raw.githubusercontent.com/jenkinsci/greenballs-plugin/master/src/main/webapp/48x48/green_anime.gif");
-    document.getElementById("jenkins-head-icon").src='https://jenkins.io/images/angry-jenkins_128.png';
-    document.getElementById("jenkins-head-icon").height='40';
 
 })();
